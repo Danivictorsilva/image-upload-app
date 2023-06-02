704e2766e7d6e08e99ac641a44ad6d7e95a5c721
Fillipe Diord <fillipe.diord@hotmail.com>
2023-06-02 15:40:31 -0300

    feat: 🎸 new changes for consumer service blob storage microsoft
    new changes for consumer service blob storage microsoft

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+import { BlobServiceClient } from "@azure/storage-blob";
+
 $(document).ready(function () {
   const inputFile = $("#picture__input");
   const pictureImage = $(".picture__image");
@@ -26,5 +28,27 @@ $(document).ready(function () {
     } else {
       $(pictureImage).attr("innerHTML", pictureImageTxt);
     }
+
+    $(".btn-send-file").on("click", async function sendArchive() {
+      try {
+        const url = "http://localhost:3000/api/upload/generate-sas-token";
+        const request = await fetch(url);
+        const response = await request.json();
+
+        // Create a new BlobServiceClient
+        const blobServiceClient = new BlobServiceClient(response.sasuri);
+
+        const containerClient = blobServiceClient.getContainerClient(response.container);
+
+        const blobClient = containerClient.getBlockBlobClient(response.filename);
+
+        const archive = $("#picture__input")[0].files[0];
+
+        blobClient.uploadBrowserData(archive);
+
+      } catch (error) {
+        console.log(error);
+      }
+    });
   });
 });
